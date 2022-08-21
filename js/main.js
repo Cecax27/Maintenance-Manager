d3262527c57afc8f468354c8142505e01b3aba2e
Carlos Cervantes <carlos27.10.98@gmail.com>
2022-08-21 16:17:24 -0500

    Initial screen

--- js/main.js
+++ js/main.js
@@ -1,42 +1,42 @@
-var sqlite3 = require(['sqlite3']);
+// var sqlite3 = require(['sqlite3']);
 
 
 const addMaintenanceCard = function (maintenance) {
     const $article = document.createElement("article"),
-        $maintenancesCards = document.getElementById('maintenancesCards');
+        $maintenancesCards = document.querySelector('.maintenancesCards');
 
     // Adding new article
     $article.classList.add('maintenance') //Adding the class
-    $article.innerHTML = `<p class="type">${maintenance.type}</p>
-        <p class="date">${maintenance.date}</p>
-        <p class="status">${maintenance.status}</p>
+    $article.innerHTML = `<p class="date">${maintenance.date}</p>
+        <p class="description">${maintenance.description}</p>
         <p class="responsible">Asignado a ${maintenance.responsible}</p>
-        <p class="comment">${maintenance.comment}</p>
-        <ul class="activities">
+        <div class="buttons">
+            <button id="more"><img src="img/glasses.svg" alt="Ver más" width="32px"></button>
+            <button id="postpone"><img src="img/timer.svg" alt="Posponer" width="22px"></button>
+            <button id="cancel"><img src="img/rechaze.svg" alt="" width="24px"></button>
+            <button id="done"><img src="img/accept.svg" alt=""width="30px"></button>
+        </div>`
 
-        </ul>
-        <button class="normal">Ver más</button>
-        <button class="delete">Eliminar</button>`
+    // $activitiesList = $article.querySelector('.activities')
 
-    $activitiesList = $article.querySelector('.activities')
-
-    for (const activity of maintenance.activities) {
-        const $newActivity = document.createElement('li')
-        $newActivity.innerHTML = `
-            <p class="name">${activity.name}</p>
-            <p class="description">${activity.description}</p>`
-        $activitiesList.appendChild($newActivity)
-    }
+    // for (const activity of maintenance.activities) {
+    //     const $newActivity = document.createElement('li')
+    //     $newActivity.innerHTML = `
+    //         <p class="name">${activity.name}</p>
+    //         <p class="description">${activity.description}</p>`
+    //     $activitiesList.appendChild($newActivity)
+    // }
 
     $maintenancesCards.appendChild($article)
 }
 
 const newMaintenance = {
+    'id' : 1,
     'type' : 'Preventivo',
-    'date' : 'Agosto 5, 2022',
+    'date' : 'Agosto 5',
     'status' : 'Programado',
     'responsible': 'Carlos Cervantes',
-    'comment': 'Mantenimiento programado para despues.',
+    'description': 'Mantenimiento programado para despues.',
     'activities': [
         {
             'name':'Inspección visual',
@@ -49,4 +49,12 @@ const newMaintenance = {
     ]
 }
 
+const newMaintenance2 = Object.assign({}, newMaintenance);
+newMaintenance2.description = "Esto es una prueba."
+newMaintenance2.date = 'Agosto 21'
+newMaintenance2.responsible = 'Adrián Ramírez'
+
 addMaintenanceCard(newMaintenance);
+addMaintenanceCard(newMaintenance2);
+addMaintenanceCard(newMaintenance);
+addMaintenanceCard(newMaintenance2);
